80bb6d02ccd7c37d9f5a06cf057241b94aece241
hilaryyates <hilryyates@gmail.com>
2022-08-08 15:29:01 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+// bug:
+
+let button = document.getElementById("enter");
+let input = document.getElementById("user-input");
+let ul = document.querySelector("ul");
+let li = undefined;
+let deleteButton = undefined;
+
+function listItem() {
+  return input.value.length;
+}
+
+function addToListAfterClick() {
+  if (listItem() > 0) {
+    createListElement();
+  }
+}
+
+function addToListAfterKeypress(event) {
+  if (listItem() > 0 && event.keyCode === 13) {
+    // more than no letters
+    createListElement();
+  }
+}
+
+function createListElement() {
+  li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  input.value = "";
+  li.addEventListener("click", () => {
+    li.classList.toggle("done");
+  });
+  createDeleteButton();
+  return li;
+}
+
+function createDeleteButton() {
+  // li = document.querySelectorAll("li");
+  deleteButton = document.createElement("button");
+  deleteButton.innerText = "delete";
+  ul.appendChild(deleteButton);
+  return deleteButton;
+}
+
+let crossOutItem = (function () {
+  li = document.querySelectorAll("li");
+  Array.from(li).forEach((item) => {
+    item.addEventListener("click", () => {
+      item.classList.toggle("done");
+    });
+  });
+})();
+
+button.addEventListener("click", addToListAfterClick);
+
+input.addEventListener("keypress", addToListAfterKeypress);
