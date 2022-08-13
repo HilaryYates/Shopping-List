6cc129ceeb64e40e78da2fa3ecd5cea205ee6ead
hilaryyates <hilryyates@gmail.com>
2022-08-12 21:05:50 -0400

    bug cross out fixed

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 let button = document.getElementById("enter");
 let input = document.getElementById("user-input");
 let ul = document.querySelector("ul");
-let li = undefined;
-let deleteButton = undefined;
 
 function listItem() {
   return input.value.length;
@@ -17,42 +15,36 @@ function addToListAfterClick() {
 function addToListAfterKeypress(event) {
   if (listItem() > 0 && event.keyCode === 13) {
     createListElement();
+    // li.addEventListener("click", () => {
+    // li.classList.toggle("done");
+    // });
   }
 }
 
 function createListElement() {
-  li = document.createElement("li");
+  let li = document.createElement("li");
   li.classList.add("text-capitalize");
   li.appendChild(document.createTextNode(input.value));
   ul.appendChild(li);
   input.value = "";
   li.addEventListener("click", () => {
+    console.log(li.innerText);
     li.classList.toggle("done");
   });
-  createDeleteButton().addEventListener("click", () => {
-    li.remove();
-    deleteButton.remove();
-  });
+  createDeleteButton();
 }
 
 function createDeleteButton() {
-  deleteButton = document.createElement("button");
+  let deleteButton = document.createElement("button");
   deleteButton.innerText = "delete";
   deleteButton.classList.add("btn", "btn-outline-danger", "delete", "btn-sml");
-  // deleteButton.style = {padding-left: "40px"}
-
-  li.appendChild(deleteButton);
-  return deleteButton;
+  // li.appendChild(deleteButton);
+  // deleteButton.addEventListener("click", () => {
+  //   li.remove();
+  //   deleteButton.remove();
+  //   console.log(li.innerText);
+  // });
 }
-
-let crossOutItem = (function () {
-  li = document.querySelectorAll("li");
-  Array.from(li).forEach((item) => {
-    item.addEventListener("click", () => {
-      item.classList.toggle("done");
-    });
-  });
-})();
 
 button.addEventListener("click", addToListAfterClick);
 
